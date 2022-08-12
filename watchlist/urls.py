fc6307b3532a1125133ad4c25be7a08139e2c7ed
Mally13 <mallymutuku13@gmail.com>
2022-08-12 20:53:30 +0300

    change settings

--- watchlist/urls.py
+++ watchlist/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.WatchlistAPIOverview, name='watchlist'),
+    path('overview', views.WatchlistAPIOverview, name='watchlist'),
     path('add/', views.add_watchlist_item, name='add_watchlist_item'),
     path('all/', views.view_watchlist_items, name='view_watchlist_items'),
     path('update/<int:pk>/', views.update_watchlist_item, name='update_watchlist_item'),
