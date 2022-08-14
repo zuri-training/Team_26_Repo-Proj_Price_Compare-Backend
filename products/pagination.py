4e2d03b6e8a017c1d5aad66e431ab54e0097732b
Mally13 <mallymutuku13@gmail.com>
2022-08-14 16:46:06 +0300

    change settings

--- products/pagination.py
+++ products/pagination.py
@@ -1,6 +1,7 @@
 from django.utils.translation import gettext_lazy as _
 
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
 
 
 class ListingPagination(PageNumberPagination):
@@ -8,5 +9,19 @@ class ListingPagination(PageNumberPagination):
     max_page_size = 100
 
     # number of items per page
-    page_size_query_description = ("Number of items per page.")
+    page_size_query_description = _("Number of items per page.")
     page_size_query_param = "items_per_page"
+
+    def get_paginated_response(self, data):
+        return Response(
+            {
+                "links": {
+                    "next": self.get_next_link(),
+                    "previous": self.get_previous_link(),
+                },
+                "total_count": self.page.paginator.count,
+                "total_pages": self.page.paginator.num_pages,
+                "item_count" : len(data),
+                "results": data,
+            }
+        )
